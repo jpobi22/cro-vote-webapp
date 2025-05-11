10b463ce7823a20c39124d8e3443f73438ec5a0c
Marin Grabovac <grabovacmarin000@gmail.com>
2025-05-11 14:26:15 +0200

    #3 Added some design and added fetching data on profile page

--- application/js/profile.js
+++ application/js/profile.js
@@ -1,5 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     getNavigation();
+    getProfileData();
+
+    async function getProfileData() {
+        try {
+
+            const jwtRes = await fetch("/api/getJWT");
+            const jwtData = await jwtRes.json();
+
+            if (jwtRes.ok && jwtData.token) {
+                const headers = new Headers();
+                headers.append("Authorization", jwtData.token);
+
+                const res = await fetch("/api/current-user", {
+                    method: "GET",
+                    headers: headers
+                });
+
+                const data = await res.json();
+    
+                if (res.ok) {
+                    document.getElementById("profile-oib").textContent = data.oib;
+                    document.getElementById("profile-name").textContent = data.name;
+                    document.getElementById("profile-surname").textContent = data.surname;
+                    document.getElementById("profile-email").textContent = data.email;
+                    document.getElementById("profile-type").textContent = data.type;
+                } else {
+                    console.error("Failed to load profile.");
+                }
+            } else {
+                console.error("Failed to fetch JWT token.");
+            }
+        } catch (err) {
+            console.error("Error fetching profile:", err);
+        }
+    }
 
     async function getNavigation() {
         try {
